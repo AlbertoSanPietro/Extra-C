a5050ce217b16742bdb24a4166440eed630af4c8
AlbertoSanPietro <albertoxsp@gmail.com>
2025-03-10 11:50:21 +0100

    added a few new files

---
+++ Signals/signalIN.c
@@ -0,0 +1,73 @@
+#ifdef __APPLE__
+	#define _DARWIN_C_SOURCE
+#elif defined(__linux__)
+	#define _GNU_SOURCE
+#endif
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <fcntl.h>
+
+
+static jmp_buf env;
+
+void sigHandler(int sig) {
+	printf("Caught your signal %d \n", sig);
+	//sleep(1);
+	longjmp(env, 1);
+
+}
+
+void setSignals() {
+	struct sigaction sa;
+	sa.sa_handler=sigHandler;
+	sa.sa_flags= SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+	
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGBUS, &sa, NULL);
+	sigaction(SIGHUP, &sa, NULL);
+	sigaction(SIGKILL, &sa, NULL);
+
+	signal (SIGTSTP, SIG_IGN);
+}
+
+void reopen_stdin() {
+	fclose(stdin);
+	stdin=fopen("/dev/tty", "r");
+}
+
+int main() {
+	char buffer[256];
+	setSignals();
+
+	if (setjmp(env)== 0){
+		printf("exit if you can..");
+	} else {
+			puts("Reopened for input");
+	}
+
+	for(;;) {
+		printf(">\t");
+		if (!fgets(buffer, sizeof(buffer), stdin)) {
+			puts("EOF detected, try again");
+			reopen_stdin();
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ Signals/signalRecovery.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <stdlib.h>
+
+#include <fcntl.h> /* O_ constants */
+#include <unistd.h> /* ftruncate */
+#include <sys/mman.h> /* mmap */
+
+void pippo();
+void pluto();
+void foo(int *);
+
+
+jmp_buf env;
+void sigHandler(int sig) {
+    switch (sig) {
+        case SIGSEGV:
+            printf("Caught SIGSEGV (Segmentation Fault). Recovering...\n");
+            break;
+        case SIGBUS:
+            printf("Caught SIGBUS (Bus Error). Recovering...\n");
+            break;
+        case SIGILL:
+            printf("Caught SIGILL (Illegal Instruction). Recovering...\n");
+            break;
+        default:
+            printf("Caught signal %d. Recovering...\n", sig);
+    }
+    
+    //printf("Caught signal %d (%s). Recovering...\n", sig, sig == SIGSEGV ? "Segmentation Fault" : "Unknown");
+    longjmp(env, 1);
+}
+
+
+void foo(int *p) {
+    p=NULL;
+    *p=42;
+}
+
+void pippo() {
+
+    printf("Attempting to cause Bus Error...\n");
+    int fd;
+    int *map;
+    int size = sizeof(int);
+    char *name = "/a";
+
+    shm_unlink(name);
+    fd = shm_open(name, O_RDWR | O_CREAT, (mode_t)0600);
+    /* THIS is the cause of the problem. */
+    /*ftruncate(fd, size);*/
+    map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    /* This is what generates the SIGBUS. */
+    *map = 0;        
+}
+
+
+__attribute__((noinline)) void illegal_instruction() {
+    __asm__ volatile ("ud2");
+}
+
+void pluto() {
+    printf("Attempting to execute an illegal instruction...\n");
+    void (*func)() = illegal_instruction;
+    func();  // Call function pointer (compiler is less likely to optimize it away)
+}
+
+void input() {
+
+}
+
+int main()
+{
+    printf("Hello world!\n");
+   int *p=malloc(sizeof(int));
+   signal(SIGSEGV, sigHandler);
+    signal(SIGBUS, sigHandler);
+    signal(SIGILL, sigHandler);
+   signal(SIGINT, sigHandler);
+    if (setjmp(env) ==0){
+     foo(p); 
+   }
+   
+   if (setjmp(env) == 0) {
+        pippo();
+    } 
+
+    if (setjmp(env) == 0) {
+        pluto();
+    } 
+
+    if(setjmp(env) != 0) {
+        input();
+    }
+   
+   return 0;
+}
